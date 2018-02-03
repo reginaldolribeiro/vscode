6fba06c6ea31cb950f2da1e7ccca059e4f4b9805
Reginaldo <reginaldolribeiro@gmail.com>
2018-02-03 17:59:09 -0200

    Classes

--- python/models.py
+++ python/models.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-class Perfil():
+class Perfil(object):
     'Classe padrao para perfis de usuarios'
 
     def __init__(self, nome, telefone, empresa):
@@ -10,3 +10,24 @@ class Perfil():
 
     def imprimir(self):
         print 'Nome: %s, Telefone: %s, Empresa %s' % (self.nome,self.telefone,self.empresa)
+
+class Data(object)        :
+
+    def __init__(self,dia,mes,ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def imprime(self):
+      print '%s/%s/%s' % (self.dia, self.mes, self.ano)
+
+class Pessoa(object):
+
+    def __init__(self,nome,peso,altura):
+        self.nome = nome
+        self.peso = float(peso)
+        self.altura = float(altura)
+
+    def imprime(self):
+        imc = self.peso/(self.altura*2)
+        print 'Imc de %s: %s' % (self.nome,imc)
