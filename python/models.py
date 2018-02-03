8ecd64a8bf9706a39f632f8ee4f338c48a129edc
Reginaldo <reginaldolribeiro@gmail.com>
2018-02-03 19:00:45 -0200

    Herança

--- python/models.py
+++ python/models.py
@@ -7,7 +7,7 @@ class Perfil(object):
         self.nome = nome
         self.telefone = telefone
         self.empresa = empresa
-        self.__curtidas = 0
+        self.__curtidas = 0        
 
     def imprimir(self):
         print 'Nome: %s, Telefone: %s, Empresa %s' % (self.nome,self.telefone,self.empresa)
@@ -18,7 +18,17 @@ class Perfil(object):
     def obter_curtidas(self):
         return self.__curtidas
 
-class Data(object)        :
+class Perfil_Vip(Perfil):
+    'Classe padrao para perfis de usuarios vips'
+
+    def __init__(self, nome, telefone, empresa, apelido):
+      super(Perfil_Vip, self).__init__(nome, telefone, empresa)
+      self.apelido = apelido
+
+    def obter_creditos(self):
+      return super(Perfil_Vip, self).obter_curtidas() * 10.0
+
+class Data(object):
 
     def __init__(self,dia,mes,ano):
         self.dia = dia
@@ -38,3 +48,21 @@ class Pessoa(object):
     def imprime(self):
         imc = self.peso/(self.altura*2)
         print 'Imc de %s: %s' % (self.nome,imc)
+
+class Conta(object):
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = float(saldo)
+
+    def calcular_imposto(self): 
+        imposto = self.saldo * 0.10
+        return imposto
+
+class ContaCorrente(Conta):
+
+    def __init__(self, titular, saldo, bonus):
+        super(ContaCorrente, self).__init__(titular, saldo)
+        self.bonus = bonus
+
+    def calcular_imposto(self):
+        return super(ContaCorrente, self).calcular_imposto() + self.bonus
